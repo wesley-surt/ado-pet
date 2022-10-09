b22c2f29afa023632d2870a5e582625f9ff11ccf
Wesley Bruno de Souza <wesleybruno.adm@gmail.com>
2022-10-08 23:13:38 -0300

    começando a validar campo de input do login
    input da pagina de login com regex atraves do atributo pattern="" da tag input do html

---
+++ javascript/execucao/index_login.js
@@ -0,0 +1,8 @@
+var inputs = document.querySelectorAll('campo__input');
+
+inputs.forEach(input => {
+    input.addEventListener('onchange', function (evt) {
+        valorDigitado = input.value;
+        valorMaiusculo
+    })
+})
