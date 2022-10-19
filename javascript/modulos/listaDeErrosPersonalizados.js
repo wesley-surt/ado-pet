e53d661216e4a4bb149527293d7931721d08ed94
Wesley Bruno de Souza <wesleybruno.adm@gmail.com>
2022-10-18 21:11:42 -0300

    adicionando validação de input na pagina perfil
    Logica de validação já existente em outras paginas. Foi feito apenas um reaproveitamento de código

--- javascript/modulos/listaDeErrosPersonalizados.js
+++ javascript/modulos/listaDeErrosPersonalizados.js
@@ -2,5 +2,8 @@ export var listaDeErrosPersonalizados = {
     email: 'O campo email está invalido.',
     nome: 'O campo nome possui alguma irregularidade.',
     senha: 'A senha deve conter no mínimo 6 digitos',
-    confirmaSenha: 'A senha informada não é idêntica a senha escolhida.'
+    confirmaSenha: 'A senha informada não é idêntica a senha escolhida.',
+    telefone: 'Formato de número celular incorreto.',
+    nomeDoAnimal: 'Este campo possui alguma irregularidade.',
+    cidade: 'Este campo possui alguma irregularidade.'
 }
